963cd247449dbe1c269f369a3dcbc25a99c63d9a
BuiHoang <bdhoang.ht@gmail.com>
2018-11-05 15:16:10 +0700

    update

--- api/interface/index.js
+++ api/interface/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const sysInfo = require('systeminformation');
 
-router.get('/', (req, res) => {
-    sysInfo.networkInterfaces(rs => {
-        res.json(rs);
-    });
+router.get('/', async (req, res) => {
+    let summary = await sysInfo.networkInterfaces();
+    let stats = await sysInfo.networkStats();
+    let connections = await sysInfo.networkConnections();
+    res.send({
+        summary,
+        stats,
+        connections
+    })
 });
 
 router.get('/stats', (req, res) => {
